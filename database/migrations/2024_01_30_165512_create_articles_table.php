86e9aeb359a0bc37c3127ec4f8787cfa2a684ec4
Jaime García <jaime.garcia@iesdonana.org>
2024-02-06 17:30:30 +0100

    Correción columna image en articles

--- database/migrations/2024_01_30_165512_create_articles_table.php
+++ database/migrations/2024_01_30_165512_create_articles_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string("title");
             $table->string("description");
             $table->string("link");
+            $table->string('image');
             $table->foreignId("user_id")->constrained();
             $table->timestamps();
         });
--- database/migrations/2024_02_01_192817_create_article-user.php
+++ database/migrations/2024_02_01_192817_create_article-user.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('article_user', function (Blueprint $table) {
             $table->foreignId('user_id')->constrained();
             $table->foreignId('article_id')->constrained();
-            $table->string('image');
             $table->primary(['user_id', 'article_id']);
             $table->timestamps();
         });
