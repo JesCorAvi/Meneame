009f553c65e4cac4295bf1b1b0227ee31d28bd9c
JesCorAvi <jesuscorderoavila1@gmail.com>
2024-02-06 22:59:41 +0100

    Cambios en los label de article y register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -40,7 +40,7 @@
         </div>
         <br>
         <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            for="file_input">Imagen</label>
+            for="file_input">Avatar</label>
         <input
             class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
             aria-describedby="file_input_help" name="file_input" id="file_input" type="file">
