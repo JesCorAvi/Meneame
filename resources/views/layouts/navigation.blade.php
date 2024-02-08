6f45719e91b7d3dc7d13721b65bc0755bfe29e75
Jaime García <jaime.garcia@iesdonana.org>
2024-02-08 21:05:49 +0100

    26.Logo y enlace general devuelven home

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,15 +5,16 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route('articles.index') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
 
                 <!-- Navigation Links ESTO HAY QUE CAMBIARLO-->
+                <!-- Navigation Links ESTO HA SIDO CAMBIADO-->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('ESTE ENLACE DEBE LLEVAR A OTRA PARTE') }}
+                    <x-nav-link :href="route('articles.index')" :active="request()->routeIs('articles.index')">
+                        {{ __('PÁGINA GENERAL') }}
                     </x-nav-link>
                 </div>
             </div>
