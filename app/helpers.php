e62e770d9515e49ea8dfb11ed2a15aa69c392cf7
Pablo Galo <pablo.galo@iesdonana.org>
2024-02-12 20:45:28 +0100

    solucion imagen comentarios

--- app/helpers.php
+++ app/helpers.php
@@ -15,7 +15,7 @@ function generar_comentarios(Comment $comment, Article $article, $contador)
                     <div class="flex items-center">
                         <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
                             <img class="mr-2 w-6 h-6 rounded-full"
-                                 src="<?= asset('storage/uploads/users/' . $comentario->user->image) ?>"
+                                 src="<?= asset($comentario->user->image) ?>"
                                  alt="<?= $comentario->user->name ?>"><?= $comentario->user->name ?></p>
                         <p class="text-sm text-gray-600 dark:text-gray-400"><time pubdate
                                                                                   datetime="2022-02-08"
